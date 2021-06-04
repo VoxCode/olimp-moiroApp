7f5848b5c987a446245756233846009c827a8118
Андрей Кезик <kezik.1992@gmail.com>
2021-06-04 16:59:19 +0300

    v 0.0.1

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -1,31 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
   styleUrls: ['./stepper.component.css']
 })
-export class StepperComponent implements OnInit {
+export class StepperComponent {
   isLinear = true;
+  isReg = false;
+  isAuth = false;
   public firstFormGroup: FormGroup = new FormGroup({
-    first: new FormControl(false, Validators.requiredTrue),
-    second: new FormControl(false, Validators.requiredTrue),
-    third: new FormControl(false, Validators.requiredTrue)
+    first: new FormControl(false, Validators.requiredTrue)
   });
 
   public secondFormGroup: FormGroup = new FormGroup({
     first: new FormControl(false, Validators.requiredTrue),
     second: new FormControl(false, Validators.requiredTrue),
-    third: new FormControl(false, Validators.requiredTrue)
+    third: new FormControl(false, Validators.requiredTrue),
+    fourth: new FormControl(false, Validators.requiredTrue)
+  });
+
+  public thirdFormGroup: FormGroup = new FormGroup({
+    first: new FormControl(false, Validators.requiredTrue),
+    second: new FormControl(false, Validators.requiredTrue),
+    third: new FormControl(false, Validators.requiredTrue),
+    fourth: new FormControl(false, Validators.requiredTrue)
+  });
+
+  public fourthFormGroup: FormGroup = new FormGroup({
+    first: new FormControl(false, Validators.requiredTrue),
+    second: new FormControl(false, Validators.requiredTrue),
+    third: new FormControl(false, Validators.requiredTrue),
   });
 
 
-  constructor(private _formBuilder: FormBuilder, private cookieService: CookieService) {
+  constructor(private _formBuilder: FormBuilder) {
   }
 
-  ngOnInit() {
+  changeIsAuth(): void {
+    this.isAuth = !this.isAuth;
+  }
 
+  changeIsReg(): void {
+    this.isReg = !this.isReg;
   }
 }
