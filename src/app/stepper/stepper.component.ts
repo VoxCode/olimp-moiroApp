75f097f56862ccbb1b8fd7be0f966f2ed2d68b0b
Андрей Кезик <kezik.1992@gmail.com>
2021-06-07 16:43:38 +0300

    v 0.0.1

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -10,6 +10,7 @@ export class StepperComponent {
   isLinear = true;
   isReg = false;
   isAuth = false;
+  st = false;
   public firstFormGroup: FormGroup = new FormGroup({
     first: new FormControl(false, Validators.requiredTrue)
   });
@@ -29,9 +30,7 @@ export class StepperComponent {
   });
 
   public fourthFormGroup: FormGroup = new FormGroup({
-    first: new FormControl(false, Validators.requiredTrue),
-    second: new FormControl(false, Validators.requiredTrue),
-    third: new FormControl(false, Validators.requiredTrue),
+    first: new FormControl(false, Validators.requiredTrue)
   });
 
 
@@ -39,10 +38,24 @@ export class StepperComponent {
   }
 
   changeIsAuth(): void {
-    this.isAuth = !this.isAuth;
+    this.isAuth = true;
+    this.isReg = false;
+    this.secondFormGroup.controls.first.patchValue(true);
+    this.secondFormGroup.controls.second.patchValue(true);
+    this.secondFormGroup.controls.third.patchValue(true);
+    this.secondFormGroup.controls.fourth.patchValue(false);
   }
 
   changeIsReg(): void {
-    this.isReg = !this.isReg;
+    this.isReg = true;
+    this.isAuth = false;
+    this.secondFormGroup.controls.first.patchValue(false);
+    this.secondFormGroup.controls.second.patchValue(false);
+    this.secondFormGroup.controls.third.patchValue(false);
+    this.secondFormGroup.controls.fourth.patchValue(true);
+  }
+
+  ste(): void {
+    this.st = true;
   }
 }
